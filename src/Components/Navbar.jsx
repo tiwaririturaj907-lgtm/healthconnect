7e937a0597e2a9d2a13b7ad43a419205312ec521
Rituraj Tiwari <tiwaririturaj907@gmail.com>
2026-06-20 00:34:11 +0530

    Fix Vercel Routing

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -4,8 +4,8 @@ function Navbar() {
   const [menuOpen, setMenuOpen] = useState(false);
 
   return (
+    <>
     <nav className="navbar">
-
       <div className="logo">
   <span>HealthConnect+</span>
 </div>
@@ -19,19 +19,23 @@ function Navbar() {
 
       <ul className={`nav-links ${menuOpen ? "active" : ""}`}>
   <li>
-    <a href="#">Home</a>
+    <a href="#"
+    onClick={() => setMenuOpen(false)}>Home</a>
   </li>
 
   <li>
-    <a href="#services">Services</a>
+    <a href="#services"
+    onClick={() => setMenuOpen(false)}>Services</a>
   </li>
 
   <li>
-    <a href="#doctors">Doctors</a>
+    <a href="#doctors"
+    onClick={() => setMenuOpen(false)}>Doctors</a>
   </li>
 
   <li>
-    <a href="#hospitals">Hospitals</a>
+    <a href="#hospitals"
+    onClick={() => setMenuOpen(false)}>Hospitals</a>
   </li>
 
   <li>
@@ -41,7 +45,7 @@ function Navbar() {
   rel="noopener noreferrer"
   className="contact-btn"
 >
-  Contact Us
+  Contact
 </a>
   </li>
   <li>
@@ -53,8 +57,14 @@ function Navbar() {
   <a href="/track-order">Track Order</a>
 </li>
 </ul>
-
     </nav>
+    {menuOpen && (
+  <div
+    className="menu-overlay"
+    onClick={() => setMenuOpen(false)}
+  />
+)}
+    </>
   );
 }
 
